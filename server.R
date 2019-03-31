78d1692c6ceec98cdcde2a5c0e5ed189670777b0
TheLoneBrit101 <thelonebrit101@gmail.com>
2019-03-31 19:51:13 +0100

    initial upload

---
+++ server.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(ECharts2Shiny)
+
+
+in50 <- function(Salary){
+    (Salary * .5) / 12
+}
+in20 <- function(Salary){
+    (Salary * .2) / 12
+}
+in30 <- function(Salary){
+    (Salary * .3) / 12
+}
+
+#dat <- data.frame(c(in50, in20, in30))
+
+# Define server logic
+shinyServer(function(input, output) {
+    
+    output$pie <- renderPlot({
+        df <- data.frame(
+            Type = c("Living", "Financial", "Flex"),
+            value = c(in50(input$Salary), in20(input$Salary), in30(input$Salary)))
+        gg <- ggplot(df, aes(x="", y=value, fill=Type)) +
+            geom_bar(width = 1, stat = "identity") + coord_polar("y", start=0)
+        plot(gg)
+})
+     output$out50 <- renderPrint({in50(input$Salary)})
+     output$out20 <- renderPrint({in20(input$Salary)})
+     output$out30 <- renderPrint({in30(input$Salary)})
+     output$rent <- renderPrint({in30(input$Salary)})
+})
+   
+ # output$out50 <- renderPrint({in50(input$Salary)})
+ # output$out20 <- renderPrint({in20(input$Salary)})
+ # output$out30 <- renderPrint({in30(input$Salary)})
+
+
---
+++ ui.R
@@ -0,0 +1,37 @@
+## Calculate how much of salary should go on rent, compared to average prices per city.
+
+library(shiny)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Bugeting"),
+  
+  # Sidebar with a slider input for number of bins 
+    sidebarPanel(
+        numericInput('Salary','Insert your annual salary (gross)', 0,min = 0, max = 300000, step = 500)),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+        p("The 50-20-30 Rule helps you build a budget by using three spending categories:"),
+        tags$ul(
+            tags$li("50% of your income should go to living expenses and essentials. This includes your rent, utilities, and things like groceries and transportation for work."),
+            tags$li("20% of your income should go to financial goals, meaning your savings, investments, and debt-reduction payments (if you have debt, such as credit card payments)"),
+            tags$li("30% of your income should be used for flexible spending. This is everything you buy that you want but don't necessarily need (like money spent on movies and travel).")),
+        p('In addition, many experts say it is best to spend no more than 30% of your monthly gross income on housing-related expenses, including rent and utilities.'),
+    
+    
+    h4('Based on your annual salary, you should be spending no more than the following monthly:'),
+    plotOutput("pie"),
+    
+    p("Living expenses:"), verbatimTextOutput("out50"), tags$br(),
+
+    p("Including 30% that should go on rent & utilities:"), verbatimTextOutput("rent"), tags$br(),
+    
+    p("Savings:"), verbatimTextOutput("out20"), tags$br(),
+    
+    p("Flex. spending:"), verbatimTextOutput("out30")
+  ))
+)
